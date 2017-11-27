258b88f0346b35764f51db2588b6aa794fd6a135
Mvelillabrandau <mathias.velilla@gmail.com>
2017-11-27 13:49:43 -0300

    Creando codigo para el diccionario .py

---
+++ extensions/logConsulta/diccionario.py
@@ -0,0 +1,14 @@
+# Leer archivo .dat (log de consulta)
+
+
+
+materias = {}
+materias["lunes"] = 0
+materias["martes"] = 1
+materias["miercoles"] = 2
+materias["jueves"] = 3
+materias["viernes"] = 4
+
+
+print materias.get("lunes")
+
